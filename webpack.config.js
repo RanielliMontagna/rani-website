2bf4b0edd8c0d22606681ec0377df665adfd053c
RanielliMontagna <ranielli@sbsistemas.com.br>
2022-05-04 21:34:49 -0300

    chore(webpack): adiciona `sourceMap` para `true`

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        parallel: true,
+      }),
+    ],
   },
 };
